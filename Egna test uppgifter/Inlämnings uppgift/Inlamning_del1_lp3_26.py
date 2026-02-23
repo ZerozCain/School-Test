9735a6292e41cd5fbd7b845c6888c65197da9838
Zeroz_Cain <engineerabbe@gmail.com>
2026-02-23 21:18:34 +0100

    Finnished work on Del1 assignment for python programing

--- Egna test uppgifter/Inlämnings uppgift/Inlamning_del1_lp3_26.py
+++ Egna test uppgifter/Inlämnings uppgift/Inlamning_del1_lp3_26.py
@@ -1,56 +1,56 @@
 # Uppdaterow 2026-02-10
 # Listor till del 1
-lgh = [['År','Månad','SE1-Fast pris 3 år','SE1-Rörligt pris','SE2-Fast pris 3 år','SE2-Rörligt pris','SE3-Fast pris 3 år','SE3-Rörligt pris','SE4-Fast pris 3 år','SE4-Rörligt pris'],
-['2024','januari',127.19,121.29,126.66,121.64,143.51,144.71,171.92,150.62],
-['2024','februari',117.75,97.07,117.06,96.70,132.46,102.05,150.66,108.56],
-['2024','mars',116.44,107.50,115.88,107.71,131.09,111.62,149.04,117.29],
-['2024','april',116.33,99.97,115.43,101.06,129.27,108.31,146.86,117.78],
-['2024','maj',119.57,60.30,118.73,60.32,131.63,67.71,148.81,100.899],
-['2024','juni',116.04,66.67,115.48,66.79,128.03,70.51,149.59,114.56],
-['2024','juli',111.63,61.86,111.22,61.70,126.05,61.81,143.33,91.61],
-['2024','augusti',112.83,45.47,112.32,45.22,127.58,45.33,145.97,93.91],
-['2024','september',109.82,47.47,109.48,49.26,124.76,56.02,142.13,73.92],
-['2024','oktober',107.98,50.03,107.73,51.18,122.62,64.03,140.88,74.26],
-['2024','november',109.29,68.10,107.81,59.54,124.10,123.63,140.23,145.87],
-['2024','december',105.70,53.35,103.55,51.27,121.35,116.01,137.43,131.13],
-['2025','januari',103.12,66.79,100.45,66.62,121.33,118.02,139.62,133.49],
-['2025','februari',104.19,53.03,101.23,55.48,121.30,136.92,143.51,172.84],
-['2025','mars',101.66,56.90,99.86,50.80,120.41,103.07,141.47,114.42],
-['2025','april',99.43,53.78,97.75,53.52,118.76,83.61,137.13,110.80],
-['2025','maj',101.55,54.43,99.89,56.09,120.31,90.52,139.41,112.10],
-['2025','juni',103.45,38.79,101.73,41.07,122.07,63.58,142.64,86.18],
-['2025','juli',103.56,52.94,102.08,55.31,123.27,82.44,144.69,94.56],
-['2025','augusti',104.51,61.21,103.21,59.78,123.70,85.67,146.23,112.96],
-['2025','september',94.80,67.38,92.96,66.25,119.07,109.34,139.30,135.41],
-['2025','oktober',104.86,54.73,103.55,53.75,125.03,118.62,146.99,127.77],
-['2025','november',103.30,88.72,101.48,85.45,122.94,131.79,144.00,145.51],
-['2025','december',100.94,76.59,98.85,77.22,118.90,101.52,139.64,119.00]]
+lgh = [["År","Månad","SE1-Fast pris 3 år","SE1-Rörligt pris","SE2-Fast pris 3 år","SE2-Rörligt pris","SE3-Fast pris 3 år","SE3-Rörligt pris","SE4-Fast pris 3 år","SE4-Rörligt pris"],
+["2024","januari",127.19,121.29,126.66,121.64,143.51,144.71,171.92,150.62],
+["2024","februari",117.75,97.07,117.06,96.70,132.46,102.05,150.66,108.56],
+["2024","mars",116.44,107.50,115.88,107.71,131.09,111.62,149.04,117.29],
+["2024","april",116.33,99.97,115.43,101.06,129.27,108.31,146.86,117.78],
+["2024","maj",119.57,60.30,118.73,60.32,131.63,67.71,148.81,100.899],
+["2024","juni",116.04,66.67,115.48,66.79,128.03,70.51,149.59,114.56],
+["2024","juli",111.63,61.86,111.22,61.70,126.05,61.81,143.33,91.61],
+["2024","augusti",112.83,45.47,112.32,45.22,127.58,45.33,145.97,93.91],
+["2024","september",109.82,47.47,109.48,49.26,124.76,56.02,142.13,73.92],
+["2024","oktober",107.98,50.03,107.73,51.18,122.62,64.03,140.88,74.26],
+["2024","november",109.29,68.10,107.81,59.54,124.10,123.63,140.23,145.87],
+["2024","december",105.70,53.35,103.55,51.27,121.35,116.01,137.43,131.13],
+["2025","januari",103.12,66.79,100.45,66.62,121.33,118.02,139.62,133.49],
+["2025","februari",104.19,53.03,101.23,55.48,121.30,136.92,143.51,172.84],
+["2025","mars",101.66,56.90,99.86,50.80,120.41,103.07,141.47,114.42],
+["2025","april",99.43,53.78,97.75,53.52,118.76,83.61,137.13,110.80],
+["2025","maj",101.55,54.43,99.89,56.09,120.31,90.52,139.41,112.10],
+["2025","juni",103.45,38.79,101.73,41.07,122.07,63.58,142.64,86.18],
+["2025","juli",103.56,52.94,102.08,55.31,123.27,82.44,144.69,94.56],
+["2025","augusti",104.51,61.21,103.21,59.78,123.70,85.67,146.23,112.96],
+["2025","september",94.80,67.38,92.96,66.25,119.07,109.34,139.30,135.41],
+["2025","oktober",104.86,54.73,103.55,53.75,125.03,118.62,146.99,127.77],
+["2025","november",103.30,88.72,101.48,85.45,122.94,131.79,144.00,145.51],
+["2025","december",100.94,76.59,98.85,77.22,118.90,101.52,139.64,119.00]]
 
-villa = [['År','Månad','SE1-Fast pris 3 år','SE1-Rörligt pris','SE2-Fast pris 3 år','SE2-Rörligt pris','SE3-Fast pris 3 år','SE3-Rörligt pris','SE4-Fast pris 3 år','SE4-Rörligt pris'],
-['2024','januari',110.06,100.48,109.58,100.93,124.09,124.70,153.33,130.32],
-['2024','februari',100.03,76.48,99.43,76.25,112.48,82.29,130.53,88.48],
-['2024','mars',98.28,82.14,97.77,90.35,110.96,100.89,127.95,128.22],
-['2024','april',83.28,78.93,90.95,80.13,100.70,88.43,123.96,97.24],
-['2024','maj',101.10,37.02,99.57,37.17,111.47,46.23,130.19,78.26],
-['2024','juni',93.92,42.97,93.13,43.07,106.92,48.20,130.05,91.82],
-['2024','juli',86.63,38.66,85.88,38.56,102.61,39.86,121.58,69.06],
-['2024','augusti',88.40,23.75,87.67,23.49,104.19,24.43,125.06,72.56],
-['2024','september',86.35,25.03,85.83,26.83,102.83,34.63,121.79,51.91],
-['2024','oktober',82.91,27.83,81.86,28.98,100.04,43.00,119.93,52.59],
-['2024','november',83.68,45.97,82.41,37.56,101.13,103.08,117.92,124.51],
-['2024','december',83.16,30.18,81.42,28.22,99.38,94.98,118.52,108.84],
-['2025','januari',81.21,45.03,79.14,44.82,98.98,97.21,118.87,112.40],
-['2025','februari',86.81,33.50,83.22,36.02,101.07,117.36,125.19,153.16],
-['2025','mars',82.87,35.91,81.12,29.88,99.59,82.70,122.68,93.57],
-['2025','april',78.60,32.89,76.54,32.83,98.04,63.33,119.11,89.87],
-['2025','maj',80.95,33.45,79.21,35.27,100.14,70.30,120.91,91.33],
-['2025','juni',82.09,17.91,79.66,20.53,101.83,43.41,123.79,65.61],
-['2025','juli',82.79,32.08,80.47,34.70,102.98,62.21,125.14,73.90],
-['2025','augusti',84.03,46.17,82.20,44.91,103.93,76.85,127.96,106.87],
-['2025','september',82.76,44.06,81.00,42.95,104.42,88.36,127.84,113.49],
-['2025','oktober',84.01,35.15,82.41,34.37,103.99,97.54,126.84,108.33],
-['2025','november',82.53,69.05,80.15,65.76,101.81,111.56,123.95,125.97],
-['2025','december',80.35,56.24,77.59,57.01,97.61,80.98,119.78,98.75]]
+villa = [["År","Månad","SE1-Fast pris 3 år","SE1-Rörligt pris","SE2-Fast pris 3 år","SE2-Rörligt pris","SE3-Fast pris 3 år","SE3-Rörligt pris","SE4-Fast pris 3 år","SE4-Rörligt pris"],
+["2024","januari",110.06,100.48,109.58,100.93,124.09,124.70,153.33,130.32],
+["2024","februari",100.03,76.48,99.43,76.25,112.48,82.29,130.53,88.48],
+["2024","mars",98.28,82.14,97.77,90.35,110.96,100.89,127.95,128.22],
+["2024","april",83.28,78.93,90.95,80.13,100.70,88.43,123.96,97.24],
+["2024","maj",101.10,37.02,99.57,37.17,111.47,46.23,130.19,78.26],
+["2024","juni",93.92,42.97,93.13,43.07,106.92,48.20,130.05,91.82],
+["2024","juli",86.63,38.66,85.88,38.56,102.61,39.86,121.58,69.06],
+["2024","augusti",88.40,23.75,87.67,23.49,104.19,24.43,125.06,72.56],
+["2024","september",86.35,25.03,85.83,26.83,102.83,34.63,121.79,51.91],
+["2024","oktober",82.91,27.83,81.86,28.98,100.04,43.00,119.93,52.59],
+["2024","november",83.68,45.97,82.41,37.56,101.13,103.08,117.92,124.51],
+["2024","december",83.16,30.18,81.42,28.22,99.38,94.98,118.52,108.84],
+["2025","januari",81.21,45.03,79.14,44.82,98.98,97.21,118.87,112.40],
+["2025","februari",86.81,33.50,83.22,36.02,101.07,117.36,125.19,153.16],
+["2025","mars",82.87,35.91,81.12,29.88,99.59,82.70,122.68,93.57],
+["2025","april",78.60,32.89,76.54,32.83,98.04,63.33,119.11,89.87],
+["2025","maj",80.95,33.45,79.21,35.27,100.14,70.30,120.91,91.33],
+["2025","juni",82.09,17.91,79.66,20.53,101.83,43.41,123.79,65.61],
+["2025","juli",82.79,32.08,80.47,34.70,102.98,62.21,125.14,73.90],
+["2025","augusti",84.03,46.17,82.20,44.91,103.93,76.85,127.96,106.87],
+["2025","september",82.76,44.06,81.00,42.95,104.42,88.36,127.84,113.49],
+["2025","oktober",84.01,35.15,82.41,34.37,103.99,97.54,126.84,108.33],
+["2025","november",82.53,69.05,80.15,65.76,101.81,111.56,123.95,125.97],
+["2025","december",80.35,56.24,77.59,57.01,97.61,80.98,119.78,98.75]]
 
 # Skriv en inledande kommentar som talar om vad programmet gör. 
 
@@ -62,32 +62,43 @@ villa = [['År','Månad','SE1-Fast pris 3 år','SE1-Rörligt pris','SE2-Fast pri
 # Returvärde: Funktionen har inget returvärde.
 def print_lista(namn):
 
-    print('Skriver ut en lista')
-    print('1: Hela listan')
-    print('2: Listan radvis')
-    print('3: Listan elementvis som matris')
-    val = input('Hur vill du skriva ut listan? ')
+    print("Skriver ut en lista")
+    print("1: Hela listan")
+    print("2: Listan radvis")
+    print("3: Listan elementvis som matris")
+    while True:
+        try:
+            val = int(input("Hur vill du skriva ut listan? "))
+            if val in range(1,4):
+                break
+            else:
+                print("\033[1;31;41m", end = "")
+                print("Ogiltig input, välj en siffra, [1-3]")
+                print("\033[0;37;40m")
+        except:
+            print("\033[1;31;41m", end = "")
+            print("Ogiltig input, välj en siffra, [1-3]")
+            print("\033[0;37;40m")
 
-    if val == '1':
-        print('\nHela listan\n') 
+    if val == 1:
+        print("\nHela listan\n") 
         print(namn) # Skriver ut hela listan i en utskrift utan radbrytning för varje rad
 
-    elif val == '2':
-        print('\nListan radvis\n')
+    elif val == 2:
+        print("\nListan radvis\n")
         for row in namn: # Skriver ut hela listan radvis med radbrytning för varje rad
             print(row)
 
-    elif val == '3':       
-        print('\nListan som matris\n')
+    elif val == 3:       
+        print("\nListan som matris\n")
         for row in namn: # Skriver ut hela listan elementvis med radbrytning för varje rad
             for item in row:
-                print(f'{item:<20}', end=" ")
-            print('')
+                print(f"{item:<20}", end=" ")
+            print("")
 
     else:
-        print('Ogiltigt val. Du kommer tillbaka till huvudmenyn.')
+        print("Ogiltigt val. Du kommer tillbaka till huvudmenyn.")
 
-#print_lista(lgh)
 # Deluppgift II: Funktioner för deluppgift II i ordning.
 # Skriv din kod här:
 def sum_list(year, column, list):
@@ -130,14 +141,14 @@ def max_value(year, column, list):
         if row[0] == str(year):
             if (type(row[column]) is float) or (type(row[column]) is int):
 
-                if row[column] >= max: # If the row[column] is greater then or equal to the max then it max that the new max as well as asigning the month of the current max
+                if row[column] >= max: # If the row[column] is greater then or equal to the max then it sets max that new value as well as assigning the month of the current value
                     max = row[column]
                     month = row[1]
     return max, month
 
 # Deluppgift V: Funktioner från deluppgift V i ordning.
 # Skriv din kod här:
-def min_value(year, column, list): # Copy of max_value() but with min instead so start out by setting it to a large number i.e. +infinity
+def min_value(year, column, list): # Copy of max_value() but with min instead so start out by setting it to a large number i.e. +infinity, as well as setting a new minimum whenerever one is found
     min = float("inf")
     month = ""
 
@@ -170,7 +181,7 @@ def analysis(year, price_list): # Makes a table of contets with, min, max and av
     
     print("-"*135)
 
-    list_offset = 2 # list_offset makes sure that "år" and "månad" doesn't get mixed in to the for-loop
+    list_offset = 2 # list_offset makes sure that "år" and "månad" doesn"t get mixed in to the for-loop
     for SE in range(0, (len(price_list[0])-2), 2): # Makes SE be 0,2,4,6,... and forever on dependiing on how long the list that is inputed is
         print(f"{price_list[0][SE+list_offset]:^15.3s}", end="")
         
@@ -191,73 +202,145 @@ def analysis(year, price_list): # Makes a table of contets with, min, max and av
 
 # Huvudprogram med Meny. Använd menyrubriker enl. uppgiftsbeskrivningen.
 # Skriv din kod här:
-def inputs():
+def year_input():
     while True:
-        year_input = int(input("Välj året som du vill arbeta med, [2024-2025]: "))
-        if year_input in range(2024,2026):
-            break
+        try:
+            year_input = int(input("Välj året som du vill arbeta med, [2024-2025]: "))
+            if year_input in range(2024,2026):
+                return year_input
+            else:
+                print("\033[1;31;41m", end = "")
+                print("Ogiltig input välj ett år, [2024-2025]")
+                print("\033[0;37;40m")
+        except:
+            print("\033[1;31;41m", end = "")
+            print("Ogiltig input, välj ett år, [2024-2025]")
+            print("\033[0;37;40m")
+        
+def column_input():
     while True:
-        column_input = (int(input("Välj en kolumn att arbeta med, 1-8: ")))
-        if column_input in range(1,9):
-            column_input += 1
-            break
+        print("SE1-Fast [1] | SE1-Rörligt [2]")
+        print("SE2-Fast [3] | SE2-Rörligt [4]")
+        print("SE3-Fast [5] | SE3-Rörligt [6]")
+        print("SE4-Fast [7] | SE4-Rörligt [8]")
+        try:
+            column_input = (int(input("Välj en kolumn att arbeta med, [1-8]: ")))
+            if column_input in range(1,9):
+                column_input += 1
+                return column_input
+            else:
+                print("\033[1;31;41m", end = "")
+                print("Ogiltig input, välj en siffra, [1-8]")
+                print("\033[0;37;40m")
+        except:
+            print("\033[1;31;41m", end = "")
+            print("Ogiltig input, välj en siffra, [1-8]")
+            print("\033[0;37;40m")
+
+def list_input():
     while True:
         list_input = (input("Välj listan som du vill arbeta med, Lägenhets lista [L] eller Villa lista [V]: ")).upper()
         if list_input == "L" or list_input == "V":
-            break
-    return year_input, column_input, list_input
+            return list_input
+        else:
+            print("\033[1;31;41m", end = "")
+            print("Ogiltig input, välj bokstav L eller V")
+            print("\033[0;37;40m")
+
+def inputs(menu_option):
+    if menu_option == 1:
+        return list_input()
+    elif menu_option == 2:
+        return year_input(), column_input(), list_input()
+    elif menu_option == 3:
+        return year_input(), column_input(), list_input()
+    elif menu_option == 4:
+        return year_input(), column_input(), list_input()
+    elif menu_option == 5:
+        return year_input(), column_input(), list_input()
+    elif menu_option == 6:
+        return year_input(), list_input()
 
 
 def menu():
-    lists = {"L" : lgh, "V" : villa}
+    lists = {"L" : lgh, "V" : villa} #Dictionary used to easily input what list to use in the functions that have need of a list as input
     print("="*135)
     print("\nProgram för att läsa in och analysera resultatet i uppgift 1 - 6\n\nDu kommer nu få några frågor som du måste svara på.\n")
     
-    year_input, column_input, list_input = inputs()
-
-    print("\n1. Skriv ut listan.")
-    print("2. Beräkna summa.")
-    print("3. Beräkna medelvärde.")
-    print("4. Hitta största värdet.")
-    print("5. Hitta minsta värdet.")
-    print("6. Analysera rörligt elpris valt år")
-    print("7. Avsluta programmet.\n")
-    print("0. Välj ny lista, år och kolumn.\n")
 
     while True:
-        base_input = int(input("Välj ett menyalternativ [0–7]: "))
-        if base_input in range(0,8):
+        print("\n1. Skriv ut listan.")
+        print("2. Beräkna summa.")
+        print("3. Beräkna medelvärde.")
+        print("4. Hitta största värdet.")
+        print("5. Hitta minsta värdet.")
+        print("6. Analysera rörligt elpris valt år")
+        print("7. Avsluta programmet.\n")
+
+        while True:
+            try:
+                base_input = int(input("Välj ett menyalternativ [1-7]: "))
+                if base_input in range(0,8):
+                    break
+            except:
+                print("\033[1;31;41m", end = "")
+                print("Ogiltig input välj en siffra mellan 1 till 7")
+                print("\033[0;37;40m")
+
+        if base_input == 1:
+            print("="*135)
+
+            list_input = inputs(base_input)
+            print_lista(lists[list_input])
+
+            print("="*135) 
+
+        elif base_input == 2:
+            print("="*135)
+
+            year_input, column_input, list_input = inputs(base_input)
+            print(f"\nSumma = {sum_list(year_input, column_input, lists[list_input]):.2f}")
+
+            print("="*135)
+
+        elif base_input == 3:
+            print("="*135)
+
+            year_input, column_input, list_input = inputs(base_input)
+            print(f"\nMedelvärde = {average_list(year_input, column_input, lists[list_input]):.2f}")
+            
+            print("="*135)
+
+        elif base_input == 4:
+            print("="*135)
+
+            year_input, column_input, list_input = inputs(base_input)
+            print(f"\nMax = {max_value(year_input, column_input, lists[list_input])[0]} ", end="")
+            print(f"i månad {max_value(year_input, column_input, lists[list_input])[1]}")
+
+            print("="*135)
+
+        elif base_input == 5:
+            print("="*135)
+
+            year_input, column_input, list_input = inputs(base_input)
+            print(f"\nMin = {min_value(year_input, column_input, lists[list_input])[0]} ", end="")
+            print(f"i månad {min_value(year_input, column_input, lists[list_input])[1]}")
+
+            print("="*135)
+
+        elif base_input == 6:
+            year_input, list_input = inputs(base_input)
+            analysis(year_input, lists[list_input])
+
+            print("="*135)
+        
+        elif base_input == 7:
+            print("="*135)
+
+            print("Program avslutat")
             break
-
-    if base_input == 1:
-        print("="*135)
-        print(lists[list_input])  
-
-    elif base_input == 2:
-        print("="*135)
-        print(f"{sum_list(year_input, column_input, lists[list_input]):.2f}")
-
-    elif base_input == 3:
-        print("="*135)
-        print(f"{average_list(year_input, column_input, lists[list_input]):.2f}")
-
-    elif base_input == 4:
-        print("="*135)
-        print(f"{max_value(year_input, column_input, lists[list_input]):.2f}")
-
-    elif base_input == 5:
-        print("="*135)
-        print(f"{min_value(year_input, column_input, lists[list_input]):.2f}")
-
-    elif base_input == 6:
-        analysis(year_input, lists[list_input])
-    
-    elif base_input == 7:
-        print("="*135)
-        print("Program avslutat")
-        exit
-
-    elif base_input == 0:
-        menu()
+        
+        base_input = None
 
 menu()
